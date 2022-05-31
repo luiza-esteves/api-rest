053511f789fad03702768a37b93d142ca4e19df1
luiza <luizaesteves987@gmail.com>
2022-05-31 15:07:26 -0300

    utilizando body-parser e tratando erro de cors

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));//mostra detalhes da rota pedida
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json())
+
+app.use((req, res, next)=>{
+    res.header('Acess-Control-Allow-Origin', '*');//servidores com acesso
+    res.header(
+        'Acess-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); //permissoes para utilizar o cabeçalho
+    
+    if(req.method=='OPTIONS'){
+        res.header(
+            'Acess-Control-Allow-Methods',
+            'PUT, POST, DELETE, GET'
+        );
+        return res.status(200).send({});
+    }
+    next();
+});
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
--- routes/produtos.js
+++ routes/produtos.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const produto ={
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
+
     res.status(201).send({
-        mensagem: 'Usando o post na rota de produtos'
+        mensagem: 'Usando o post na rota de produtos',
+        produtoCriado: produto
     });
 });
 
